04ceb0d8f85a36f404b822fd0c9529872637758e
GWorsnop <georgeworsnop@gmail.com>
2022-08-23 16:02:45 +0100

    Change params on API request to filter search

--- src/components/api-interaction/getComments.js
+++ src/components/api-interaction/getComments.js
@@ -3,7 +3,10 @@ import axios from "axios";
 export default function getComments(review_id) {
   return axios
     .get(
-      `https://nc-games-gworsnop.herokuapp.com/api/reviews/${review_id}/comments`
+      `https://nc-games-gworsnop.herokuapp.com/api/reviews/${review_id}/comments`,
+      {
+        params: { limit: 100 },
+      }
     )
     .then((res) => {
       return res.data.comments;
--- src/components/api-interaction/getReviews.js
+++ src/components/api-interaction/getReviews.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-export default function getReviews() {
+export default function getReviews({ category, sort_by, order, limit, p }) {
   return axios
     .get(`https://nc-games-gworsnop.herokuapp.com/api/reviews`, {
-      params: { limit: 100 },
+      params: { category, sort_by, order, limit, p },
     })
     .then((res) => {
       return res.data.reviews;
