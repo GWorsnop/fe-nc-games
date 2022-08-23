ee3d16afd388dc392aba17e415529f4cadb41173
GWorsnop <georgeworsnop@gmail.com>
2022-08-23 11:37:40 +0100

    Create dropdown that shows comments for review

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,61 +1,82 @@
 import { useState, useEffect } from "react";
 import { useContext } from "react";
 import { UserContext } from "./UserContext";
+import getComments from "./api-interaction/getComments";
+import CommentVotesButton from "./CommentVotesButton";
+import getUsers from "./api-interaction/getUsers";
 
-function ShowComments() {
-  const [showComments, setShowComments] = useState(false);
+function Comments({ review_id }) {
+  const [comments, setComments] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [users, setUsers] = useState([]);
 
-  return (
-    <>
-      <button
-        className="btn bg-white hover:bg-green-100 text-black text-xs"
-        onClick={(e) => {
-          setShowComments(true);
-        }}
-      >
-        Show Comments
-      </button>
-      <button
-        className="btn bg-white hover:bg-red-100 text-black text-xs"
-        onClick={(e) => {
-          setShowComments(false);
-        }}
-      >
-        Hide Comments
-      </button>
-      {showComments ? <Comments /> : null}
-    </>
-  );
-}
+  useEffect(() => {
+    setIsLoading(true);
+    getComments(review_id).then((data) => {
+      setComments(data);
+      setIsLoading(false);
+    });
+  }, [review_id]);
 
-function Comments() {
-  return (
-    <div className="relative grid grid-cols-1 gap-4 p-4 mb-8 border rounded-lg bg-white shadow-lg">
-      <div className="relative flex gap-4">
-        <img
-          src="https://icons.iconarchive.com/icons/diversity-avatars/avatars/256/charlie-chaplin-icon.png"
-          className="relative rounded-lg -top-8 -mb-4 bg-white border h-20 w-20"
-          alt="chaplin"
-          loading="lazy"
-        />
-        <div className="flex flex-col w-full">
-          <div className="flex flex-row justify-between">
-            <p className="relative text-xl whitespace-nowrap truncate overflow-hidden">
-              COMMENTOR
-            </p>
-            <a className="text-gray-500 text-xl" href="#">
-              <i className="fa-solid fa-trash"></i>
-            </a>
-          </div>
-          <p className="text-gray-400 text-sm">20 April 2022, at 14:88 PM</p>
+  useEffect(() => {
+    setIsLoading(true);
+    getUsers().then((data) => {
+      setUsers(data);
+      setIsLoading(false);
+    });
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className="m-auto">
+        <h3 className="text-xl font-bold"> Comments: </h3>
+        <br />
+        <div>
+          <p className="loader"></p>
         </div>
       </div>
-      <p className="-mt-4 text-gray-500">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. <br />
-        Maxime quisquam vero adipisci beatae voluptas dolor ame.
-      </p>
-    </div>
-  );
+    );
+  } else
+    return (
+      <>
+        {comments.map((comment, i) => {
+          let userUrl = "";
+          users.forEach((item) => {
+            if (item.username == comment.author) {
+              userUrl = item.avatar_url;
+            }
+          });
+
+          return (
+            <div
+              key={comment.comment_id}
+              className="relative grid grid-cols-1 gap-4 p-4 mb-8 border rounded-lg bg-white shadow-lg"
+            >
+              <div className="relative flex gap-4">
+                <img
+                  src={userUrl}
+                  className="relative rounded-lg -top-8 -mb-4 bg-white border h-20 w-20"
+                  alt="placeholder"
+                />
+                <div className="flex flex-col w-full">
+                  <div className="flex flex-row justify-between">
+                    <p className="relative text-xl whitespace-nowrap truncate overflow-hidden">
+                      {comment.author}
+                    </p>
+                  </div>
+                  <p className="text-gray-400 text-sm">{comment.created_at}</p>
+                </div>
+              </div>
+              <p className="-mt-4 text-gray-500">{comment.body}</p>
+              <CommentVotesButton
+                comment_id={comment.comment_id}
+                originalVote={comment.votes}
+              />
+            </div>
+          );
+        })}
+      </>
+    );
 }
 
-export default ShowComments;
+export default Comments;
