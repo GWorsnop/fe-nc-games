e0dc493fc88b4ea0e5f0d15da2f72ae78b7ed907
George Worsnop <georgeworsnop@gmail.com>
2023-06-01 22:30:30 +0100

    change getReviews api error

--- src/components/api-interaction/getReviews.js
+++ src/components/api-interaction/getReviews.js
@@ -1,10 +1,12 @@
 import axios from "axios";
 
 export default function getReviews() {
-  return axios.get(`http://54.86.179.94:8080/api/events`).then((res) => {
-    console.log(res.data.events);
-    return res.data.events;
-  });
+  return axios
+    .get(`https://nc-games-backend-nfl0.onrender.com/api/events`)
+    .then((res) => {
+      console.log(res.data.events);
+      return res.data.events;
+    });
 }
 
 getReviews();
