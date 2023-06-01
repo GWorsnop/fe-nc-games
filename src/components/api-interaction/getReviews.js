0dd7b3a84dc1dbdf297367269357e46b5d976049
George Worsnop <georgeworsnop@gmail.com>
2023-06-01 22:33:40 +0100

    reformat getReviews

--- src/components/api-interaction/getReviews.js
+++ src/components/api-interaction/getReviews.js
@@ -2,10 +2,10 @@ import axios from "axios";
 
 export default function getReviews() {
   return axios
-    .get(`https://nc-games-backend-nfl0.onrender.com/api/events`)
+    .get(`https://nc-games-backend-nfl0.onrender.com/api/reviews`)
     .then((res) => {
-      console.log(res.data.events);
-      return res.data.events;
+      console.log(res.data.reviews);
+      return res.data.reviews;
     });
 }
 
